10faa99109b2360df1b9e064da0ede22701731f0
pajaro <darkdrei88@gmail.com>
2018-03-23 03:02:01 -0500

    Add package in file principal

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3977;
 
-mongoose.Promise = global.Promise; //Eliminar mensaje de mongose a el instanciarse
+mongoose.Promise = global.Promise; //Eliminar mensaje de mongose a el instanciarse;
 mongoose.connect('mongodb://localhost:27017/curso_mean', (err, res) => {
     if(err){
       throw err;
     }else{
-      console.log("Ready........");
+      console.log("Ready....");
       app.listen(port, function(){
         console.log("Servidor del api res de musica escuchando en hhtp://localhost:"+port);
       })
