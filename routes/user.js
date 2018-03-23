8ab7e8797eaeda5139b685c017f1d6622a874d27
pajaro <darkdrei88@gmail.com>
2018-03-23 03:39:44 -0500

    Add middleware in the routes

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,9 @@ var express = require('express');
 var user_controller = require('../controllers/user');
 
 var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
 
-api.get('/probando-controlador', user_controller.pruebas);
+api.get('/probando-controlador',md_auth.ensureAuth, user_controller.pruebas);
 api.post('/register', user_controller.saveUser);
 api.post('/login', user_controller.loginUser);
 
