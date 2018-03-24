2bc167abbfc5a9ff9956f7a5c600e58c507e2c65
pajaro <darkdrei88@gmail.com>
2018-03-24 10:57:35 -0500

    Ruta de mostrar imagen

--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,14 @@ var user_controller = require('../controllers/user');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir:'./uploads/users'});
+
 api.get('/probando-controlador',md_auth.ensureAuth, user_controller.pruebas);
 api.post('/register', user_controller.saveUser);
 api.post('/login', user_controller.loginUser);
+api.put('/update-user/:id',md_auth.ensureAuth, user_controller.updateUser);
+api.post('/upload-image/:id',[md_auth.ensureAuth, md_upload], user_controller.uploadImage);
+api.get('/get-image-user/:imageFile', user_controller.getImageFile);
 
 module.exports = api;
