55bafb43ff2f3d8ffb108c4be8bc06fd218d5b39
pajaro <darkdrei88@gmail.com>
2018-03-25 02:57:13 -0500

    configuracion de cabecera

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //Configuracion de cabeceras http
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
 
 //Rutas base
 app.use('/api', user_routes); //Especie de midaware
