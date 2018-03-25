a264295257bba2d9722ee3a6785f907f2e00a4b1
pajaro <darkdrei88@gmail.com>
2018-03-24 22:46:12 -0500

    Routes de albunes

--- routes/album.js
+++ routes/album.js
@@ -1,3 +1,23 @@
 /**
  * Created by dark on 24/03/18.
  */
+'use strict'
+
+var express = require('express');
+var AlbumControler = require('../controllers/album');
+var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir:'./uploads/artists'});
+
+api.get('/album',md_auth.ensureAuth, AlbumControler.getAlbum);
+//api.post('/artist',md_auth.ensureAuth, ArtisControler.saveArtist);
+//api.get('/artist/:id',md_auth.ensureAuth, ArtisControler.getArtist);
+//api.get('/artists/:page?',md_auth.ensureAuth, ArtisControler.getArtists);
+//api.put('/artist/:id',md_auth.ensureAuth, ArtisControler.updateArtist);
+//api.delete('/artist/:id',md_auth.ensureAuth, ArtisControler.deleteArtist);
+//api.post('/upload-image-artist/:id',[md_auth.ensureAuth, md_upload], ArtisControler.uploadImage);
+//api.get('/get-image-artist/:imageFile', ArtisControler.getImageFile);
+
+
+module.exports =api;
