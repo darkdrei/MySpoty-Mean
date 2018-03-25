28c6e3b1f5afed32bb33afdf37c2ef2dbaba87f2
pajaro <darkdrei88@gmail.com>
2018-03-24 22:39:19 -0500

    Controlador de album

---
+++ controllers/album.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dark on 24/03/18.
+ */
+
+'use strict'
+
+var path = require('path');
+var fs = require('fs');
+var Artist = require('../models/artist');
+var Album = require('../models/album');
+var Song = require('../models/song');
+var mongoosePaginate = require('mongoose-pagination');
+
