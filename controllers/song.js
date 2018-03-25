44fd9e6b04924c33753ecfd6932a6b1e97cc3e9a
pajaro <darkdrei88@gmail.com>
2018-03-25 02:10:13 -0500

    Funcionalidad de save song

--- controllers/song.js
+++ controllers/song.js
@@ -9,3 +9,36 @@ var Artist = require('../models/artist');
 var Album = require('../models/album');
 var Song = require('../models/song');
 var mongoosePaginate = require('mongoose-pagination');
+
+
+function getSong(request, response) {
+    response.status(200).send({menssage:'Controlador de cancions'});
+}
+
+function saveSong(request, response) {
+    var song = new Song();
+    var params = request.body;
+    song.name = params.name;
+    song.number = params.number;
+    song.duration = params.duration;
+    song.album = params.album;
+    song.file = 'null';
+    song.save((err, songStored)=>{
+        if(err){
+            response.status(500).send({menssage:'Erros a el guardar el song'});
+        }else{
+            if(!songStored){
+                response.status(404).send({menssage: 'Song no guardado.'})
+            }else{
+                response.status(200).send({message: songStored});
+            }
+        }
+
+    });
+}
+
+
+module.exports={
+    getSong,
+    saveSong
+};
---
+++ routes/song.js
@@ -0,0 +1,26 @@
+/**
+ * Created by dark on 25/03/18.
+ */
+/**
+ * Created by dark on 24/03/18.
+ */
+'use strict'
+
+var express = require('express');
+var SongControler = require('../controllers/song');
+var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir:'./uploads/songs'});
+
+api.get('/song',md_auth.ensureAuth, SongControler.getSong);
+api.post('/song',md_auth.ensureAuth, SongControler.saveSong);
+//api.get('/artist/:id',md_auth.ensureAuth, ArtisControler.getArtist);
+//api.get('/albums/:artist',md_auth.ensureAuth, AlbumControler.getAlbums);
+//api.put('/album/:id',md_auth.ensureAuth, AlbumControler.updateAlbum);
+//api.delete('/album/:id',md_auth.ensureAuth, AlbumControler.deleteAlbum);
+//api.post('/upload-image-album/:id',[md_auth.ensureAuth, md_upload], AlbumControler.uploadImage);
+//api.get('/get-image-album/:imageFile', AlbumControler.getImageFile);
+
+
+module.exports =api;
