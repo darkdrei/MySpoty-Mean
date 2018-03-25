8d2122673c81fae5700edfd562fa064c5d850fe7
pajaro <darkdrei88@gmail.com>
2018-03-24 23:12:56 -0500

    ruta de listar albunes de un artista

--- routes/album.js
+++ routes/album.js
@@ -10,10 +10,10 @@ var md_auth = require('../middlewares/authenticated');
 var multipart = require('connect-multiparty');
 var md_upload = multipart({uploadDir:'./uploads/albums'});
 
-api.get('/album',md_auth.ensureAuth, AlbumControler.getAlbum);
-//api.post('/artist',md_auth.ensureAuth, ArtisControler.saveArtist);
+api.get('/album/:id',md_auth.ensureAuth, AlbumControler.getAlbum);
+api.post('/album',md_auth.ensureAuth, AlbumControler.saveAlbum);
 //api.get('/artist/:id',md_auth.ensureAuth, ArtisControler.getArtist);
-//api.get('/artists/:page?',md_auth.ensureAuth, ArtisControler.getArtists);
+api.get('/albums/:artist',md_auth.ensureAuth, AlbumControler.getAlbums);
 //api.put('/artist/:id',md_auth.ensureAuth, ArtisControler.updateArtist);
 //api.delete('/artist/:id',md_auth.ensureAuth, ArtisControler.deleteArtist);
 //api.post('/upload-image-artist/:id',[md_auth.ensureAuth, md_upload], ArtisControler.uploadImage);
