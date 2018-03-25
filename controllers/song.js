7b37f8e3c525e1e1bfc0e545755334126b578a4b
pajaro <darkdrei88@gmail.com>
2018-03-25 01:44:45 -0500

    Controlador de song

--- controllers/song.js
+++ controllers/song.js
@@ -1,3 +1,11 @@
 /**
  * Created by dark on 25/03/18.
  */
+'use strict'
+
+var path = require('path');
+var fs = require('fs');
+var Artist = require('../models/artist');
+var Album = require('../models/album');
+var Song = require('../models/song');
+var mongoosePaginate = require('mongoose-pagination');
